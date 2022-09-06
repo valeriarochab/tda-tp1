a102665e47c24ecad8190b2250c9a091ea7ab029
valeriarochab <valeria.rocha@despegar.com>
2022-09-06 20:01:57 -0300

    add solution and tests files

--- main.py
+++ main.py
@@ -1,20 +1,79 @@
 import os
+import sys
+import heapq
 
 partners = {}
 
 
+def main():
+    arguments = sys.argv[1:]
+    n = int(arguments[0])
+    file_name = arguments[1]
 
-def main(n, file_name):
+    if n <= 4:
+        print("n debe ser mayor a 4")
+        return
+
+    get_partners(file_name)
+    partners_list = create_heap()
+
+    while len(partners.items()) >= 5:
+        candidate = partners_list[0]
+        if candidate[0] <= 3:
+            del partners[candidate[1]]
+            remove_partner(candidate[1])
+            partners_list = create_heap()
+        else:
+            break
+
+    print(partners)
+
+
+def get_partners(file_name):
     current_path = os.path.dirname(os.path.abspath(__file__))
     with open("{}/{}".format(current_path, file_name)) as fp:
         lines = fp.readlines()
         for line in lines:
+            line = line.replace("\n", "")
             elements = line.split(",")
             partner_number = elements[0]
             partner_name = elements[1]
             known_partners = elements[2:]
-            partners[partner_number] = (partner_name, known_partners)
+            known_partners_length = len(known_partners)
 
-    print(partners)
+            print(known_partners)
+            completed_partners = [0] * int(known_partners[-1])
 
-main(5, "test.txt")
+            for x in known_partners:
+                completed_partners[int(x)-1] = 1
+
+            partners[partner_number] = (partner_name, known_partners_length, completed_partners)
+
+
+def create_heap():
+    partners_list = [(v[1], k) for k, v in partners.items()]
+    heapq.heapify(partners_list)
+    return partners_list
+
+
+def remove_partner(partner_number):
+    for key, value in partners.items():
+        partner_name = value[0]
+        known_partners_length = value[1]
+        completed_partners = value[2]
+        if int(partner_number) <= len(completed_partners) and completed_partners[int(partner_number) - 1] == 1:
+            known_partners_length = known_partners_length - 1
+            completed_partners[int(partner_number)-1] = 0
+            partners[key] = (partner_name, known_partners_length, completed_partners)
+
+
+#def save_result(list):
+#    file = open("result.txt", "w")
+#    for idx, pair in enumerate(list):
+#        file.write("{} {}\n".format(pair[0], idx+1))
+#        file.write("{} {}\n".format(pair[1], idx+1))
+#    file.close()
+
+
+
+main()
